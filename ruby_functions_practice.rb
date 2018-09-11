8067e9a12185a2d75b132b8bb5789b9009f5dca4
Kris Mac <email@krismac.com>
2018-09-11 15:44:57 +0100

    up to futher

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -33,14 +33,53 @@
  #   return str_1.to_i + str_2.to_i
  # end
 
-def number_to_full_month_name (month_number)
-  case month_number
+# def number_to_full_month_name (month_number)
+#   case month_number
+# 	when 1
+# 	  return "January"
+# 	when 2
+# 	  return "February"
+# 	when 3
+# 	  return "March"
+#   when 9
+#     return "September"
+#   else
+#     return "failed"
+#   end
+# end
+#
+# def number_to_short_month_name (month_short)
+#   case month_short
+# 	when 1
+# 	  return "Jan"
+# 	when 2
+# 	  return "Feb"
+# 	when 3
+# 	  return "Mar"
+#   when 9
+#     return "Sep "
+#   else
+#     return "failed"
+#   end
+# end
+
+
+
+
+def number_to_short_month_name (month_short)
+  case month_short
 	when 1
-	  return "January"
+	  return "Jan"
 	when 2
-	  return "February"
+	  return "Feb"
 	when 3
-	  return "March"
+	  return "Mar"
+  when 4
+    return "Apr"
+  when 9
+    return "Sep "
+  when 10
+    return "Oct"
   else
     return "failed"
   end
