a703a5a009ebe4525a391e1f90fb3370cf8512f4
Kris Mac <email@krismac.com>
2018-09-11 15:31:56 +0100

    Full month name 1

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -15,6 +15,33 @@
 #   return num_1 * num_2
 # end
 
-def divide(num_1, num_2)
-  return num_1 / num_2
+# def divide(num_1, num_2)
+#   return num_1 / num_2
+# end
+
+
+# def length_of_string(input_string)
+#   return input_string.length
+# end
+
+ # def join_string(str_1, str_2)
+ #   return str_1 + str_2
+ # end
+
+ #
+ # def add_string_as_number (str_1, str_2)
+ #   return str_1.to_i + str_2.to_i
+ # end
+
+def number_to_full_month_name (month_number)
+  case month_number
+	when 1
+	  return "January"
+	when 2
+	  return "February"
+	when 3
+	  return "March"
+  else
+    return "failed"
+  end
 end
