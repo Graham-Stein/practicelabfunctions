e83dcf9c324377d3251f90650fc05c19d3e3e50e
Kris Mac <email@krismac.com>
2018-09-11 14:57:23 +0100

    Divide complete

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,3 +1,20 @@
-def return_10()
-return 10
+# def return_10()
+# return 10
+# end
+#
+#
+# def add(num1,num2)
+#   return num1+num2
+# end
+#
+# def subtract(num_1, num_2)
+#   return num_1 - num_2
+# end
+#
+# def multiply(num_1, num_2)
+#   return num_1 * num_2
+# end
+
+def divide(num_1, num_2)
+  return num_1 / num_2
 end
